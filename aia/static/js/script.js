b147850df9911146d79fbc5fbcef7fe7ae14c094
marlonargolo <110413185+marlonargolo@users.noreply.github.com>
2024-11-04 18:35:15 -0300

    Delete aia directory

--- aia/static/js/script.js
+++
@@ -1,5 +0,0 @@
-// Exemplo simples de alerta ao submeter um formulário
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário submetido!');
-});
